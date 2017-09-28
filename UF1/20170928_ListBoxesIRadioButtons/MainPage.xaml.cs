e8c3e9091049a824238a287f8a41a7c1ff609b7e
Bernat Orellana <borellan@xtec.cat>
2017-09-28 20:24:36 +0200

    mostrar les respostes

--- UF1/20170928_ListBoxesIRadioButtons/MainPage.xaml.cs
+++ UF1/20170928_ListBoxesIRadioButtons/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _20170928_ListBoxesIRadioButtons.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -29,7 +30,45 @@ namespace _20170928_ListBoxesIRadioButtons
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            
+            List<Pregunta> preguntes = Pregunta.GetPreguntes();
+            lsbPreguntes.Items.Clear(); // netejar tota la llista d'ítems actual
+            // Recorregut sobre totes les preguntes
+            foreach (Pregunta p in preguntes) {
+                ListBoxItem item = new ListBoxItem(); // Creem un ítem
+                item.Content = p.Numero; // assignem el text de l'ítem al número de la pregunat
+                lsbPreguntes.Items.Add(item);
+            }
+            if (preguntes.Count > 0)
+            {
+                lsbPreguntes.SelectedIndex = 0;
+            }
+        }
+
+        private void lsbPreguntes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int indexPreguntaSeleccionada = lsbPreguntes.SelectedIndex;
+            mostrarPregunta(indexPreguntaSeleccionada);
+        }
+
+        private void mostrarPregunta(int indexPreguntaSeleccionada)
+        {
+            List<Pregunta> preguntes = Pregunta.GetPreguntes();
+            Pregunta p = preguntes[indexPreguntaSeleccionada];
+            txbPregunta.Text = p.TextPregunta;
+            StackPanel panellSeleccionat = p.EsMultiresposta ? stpMultireposta : stpMonoreposta;
+            StackPanel panellAmagat = p.EsMultiresposta ?  stpMonoreposta: stpMultireposta;
+            panellSeleccionat.Visibility = Visibility.Visible;
+            panellAmagat.Visibility = Visibility.Collapsed;
+
+            int i = 0;
+            char lletra = 'a';
+            foreach ( string resposta in p.Respostes)
+            {
+                ButtonBase b = (ButtonBase)panellSeleccionat.Children[i];
+                b.Content = lletra+") "+resposta;
+                i++;
+                lletra++;
+            }
         }
     }
 }
