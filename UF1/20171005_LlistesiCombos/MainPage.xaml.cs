6cf7c9103615f8e9f5d835f20b23c4feb9e5e5da
Bernat Orellana <borellan@xtec.cat>
2017-10-06 17:40:26 +0200

    llistes i combos

---
+++ UF1/20171005_LlistesiCombos/MainPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _20171005_LlistesiCombos
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // AQUI NO PROGRAMEU SIUSPLAU  ;-)
+            // ( SOTA LA VOSTRA RESPONSABILITAT)
+            /*
+                                 Envoyer par email 	
+
+                                         _,.-------.,_
+                                     ,;~'             '~;,
+                                   ,;                     ;,
+                                  ;                         ;
+                                 ,'                         ',
+                                ,;                           ;,
+                                ; ;      .           .      ; ;
+                                | ;   ______       ______   ; |
+                                |  `/~"     ~" . "~     "~\'  |
+                                |  ~  ,-~~~^~, | ,~^~~~-,  ~  |
+                                 |   |        }:{        |   |
+                                 |   l       / | \       !   |
+                                 .~  (__,.--" .^. "--.,__)  ~.
+                                 |     ---;' / | \ `;---     |
+                                  \__.       \/^\/       .__/
+                                   V| \                 / |V
+                                    | |T~\___!___!___/~T| |
+                                    | |`IIII_I_I_I_IIII'| |
+                                    |  \,III I I I III,/  |
+                                     \   `~~~~~~~~~~'    /
+                                       \   .       .   /     -dcau (4/15/95)
+                                         \.    ^    ./
+                                           ^~~~^~~~^
+
+
+             **/
+        }
+
+        private ObservableCollection<string> mProvincies;
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*cboProvincies.Items.Add("Barcelona");
+            cboProvincies.Items.Add("Lleida");
+            cboProvincies.Items.Add("Girona");
+            cboProvincies.Items.Add("Tarragona");
+            */
+            mProvincies = new ObservableCollection<string>();
+            mProvincies.Add("Barcelona");
+            mProvincies.Add("Lleida");
+            mProvincies.Add("Girona");
+            mProvincies.Add("Tarragona");
+
+            cboProvincies.ItemsSource = mProvincies;
+
+        }
+
+        private void btnAfegir_Click(object sender, RoutedEventArgs e)
+        {
+            string novaProvincia = txtProvincia.Text;
+            if (!mProvincies.Contains(novaProvincia))
+            {
+                mProvincies.Add(novaProvincia);
+            }
+        }
+    }
+}
