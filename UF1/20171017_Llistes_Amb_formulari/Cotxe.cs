a45fdfe8c335aa6d21ce28afbf77ad37b4de968e
Bernat Orellana <borellan@xtec.cat>
2017-10-17 19:30:45 +0200

    - Llistes - Nou projecte llistes + formulari

---
+++ UF1/20171017_Llistes_Amb_formulari/Cotxe.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace _20171010_Llistes
+{
+    class Cotxe
+    {
+
+        public static ObservableCollection<Cotxe>
+            getCotxes()
+        {
+            ObservableCollection<Cotxe> llistaCotxes =
+                new ObservableCollection<Cotxe>();
+
+            llistaCotxes.Add(new Cotxe("1232-JLK", "2008", "Peugeot", Colors.Aquamarine));
+            llistaCotxes.Add(new Cotxe("2345-PTR", "Ibiza", "Seat", Colors.Red));
+            llistaCotxes.Add(new Cotxe("8234-PTR", "Leon", "Seat", Colors.Green));
+            llistaCotxes.Add(new Cotxe("6523-YYY", "Meriva", "Opel", Colors.Blue));
+
+            return llistaCotxes;
+        }
+
+        //----------------------------------------------------
+        public Cotxe(String pMatricula, 
+            String pModel, 
+            String pMarca, 
+            Color pColor)
+        {
+            Matricula = pMatricula;
+            Model = pModel;
+            Marca = pMarca;
+            ColorCotxe = pColor;
+        }
+        //----------------------------------------------------
+        #region Propietats
+
+        //----------------------------------------
+        private String mMatricula;
+
+        public String Matricula
+        {
+            get { return mMatricula; }
+            set { mMatricula = value; }
+        }
+        //----------------------------------------
+        private String mMarca;
+
+        public String Marca
+        {
+            get { return mMarca; }
+            set { mMarca = value; }
+        }
+        //----------------------------------------
+        private String mModel;
+
+        public String Model
+        {
+            get { return mModel; }
+            set { mModel = value; }
+        }
+        //----------------------------------------
+
+        private Color mColor;
+
+        public Color ColorCotxe
+        {
+            get { return mColor; }
+            set { mColor = value; }
+        }
+        //----------------------------------------
+        public Brush Pinzell
+        {
+            get { return new SolidColorBrush(mColor); }
+        }
+
+
+        public override string ToString()
+        {
+            return "M:" + Matricula + " , marca:" + Marca + ", model:" + Model;
+        }
+
+
+        public String RutaImatgeMarca
+        {
+            get { return "Assets/" + Marca + ".png"; }
+        }
+
+        #endregion
+    }
+}
